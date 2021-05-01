f03fe3dbb3fc62c6fab11b982c46b6b4301a675b
Omar-aly <omar.aly@sci.ku.edu.kw>
2021-05-02 00:05:56 +0300

    Phase1 files

---
+++ src/nonPremptiveSJF.c
@@ -0,0 +1,42 @@
+/* This is the source code of the non-preemtive Shortest Job First Scheduling */
+#include "project.h"
+#include "die_with_error.h"
+void die_with_error(char *msg);
+
+int main() {
+	int burstTime[ASIZE] = {};
+	int processes[ASIZE] = {};
+	int waitingTime[ASIZE] = {};
+	int turnAroundTime[ASIZE] = {};
+	int i, j, fd;
+	int k, l, closing_descriptor = 0;
+	char *region;
+	struct stat info;
+	char buff[MAXBUF];
+	bzero(buff, sizeof(buff));
+
+	fd = open("file.txt", O_RDONLY, 0);
+
+	if (fd < 0) 
+		die_with_error("Error opening file");
+
+	fstat(fd, &info);
+	region = mmap(NULL, info.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+	
+	for (k = 0, l = 0; k < info.st_size; k++, l++)
+		buff[l] = region[k];
+		
+	printf("%s\n", buff);
+	printf("%c\n%c\n%c\n%c\n%c\n", buff[0], buff[1], buff[2], buff[3], buff[4]);
+
+
+
+	closing_descriptor = close(fd);
+	if (closing_descriptor < 0)
+		die_with_error("Error closing the file");
+
+	munmap(region, sizeof(region));
+		
+	return 0;
+
+}
