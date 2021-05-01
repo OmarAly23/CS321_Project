203617fe8b1e0df79d36a2533e9c9fc50e9b7e60
OmarAly23 <aly.omar@cs.ku.edu.kw>
2021-05-02 02:28:14 +0300

    Completed the algorithm's implementation

--- src/nonPremptiveSJF.c
+++ src/nonPremptiveSJF.c
@@ -8,7 +8,8 @@ int main() {
 	int processes[ASIZE] = {};
 	int waitingTime[ASIZE] = {};
 	int turnAroundTime[ASIZE] = {};
-	int i, j, fd, numOfProcesses;
+	int i, j, fd, numOfProcesses, position, tempVal, total=0;
+	double waitingAvg, turnAroundAvg;
 	int k, l, closing_descriptor = 0;
 	char *region;
 	char *temp;
@@ -28,7 +29,7 @@ int main() {
 	for (k = 0, l = 0; k < info.st_size; k++, l++)
 		buff[l] = region[k];
 		
-	printf("%s\n", buff);
+	//printf("%s\n", buff);
 	/* Extracting the number of processes from the file */
 	temp = strtok(buff, "\n");
 	numOfProcesses = atoi(temp);
@@ -48,7 +49,58 @@ int main() {
 		processes[p] = p+1;
 	}
 	
+	/* Sorting algorithm of the burst time */
+	/* The burst times are being sorted using the selection sort algorithm */
+	for (i = 0; i < numOfProcesses; i++) {
+		position = i;
+		for (j = i; j < numOfProcesses; j++) {
+			if (burstTime[j] < burstTime[position])
+				position = j;
+		}
 
+		/* Swapping */
+		tempVal = burstTime[i];
+		burstTime[i] = burstTime[position];
+		burstTime[position] = tempVal;
+
+		tempVal = processes[i];
+		processes[i] = processes[i];
+		processes[position] = tempVal;
+	}
+
+	/* The waiting time of the first element is always zero */
+	waitingTime[0] = 0;
+
+	/* The waiting time is computed by adding the burst time with the waiting time */
+	for (i = 1; i < numOfProcesses; i++) {
+		waitingTime[i] = 0;
+		for (j = 0; j < i; j++)
+			waitingTime[i] += burstTime[j];
+		/* The total variable contains the sun of all the waiting time */
+		total += waitingTime[i];
+	}
+	/* 
+	 * Now that we have the sum of the waiting time and the number of processes 
+	 * We can compute the average of the waiting time
+	 */
+	waitingAvg = (double)total / numOfProcesses;
+
+	/* re-initialize the variable to be used again for the turn around time calculation */
+	total = 0;
+	
+	printf("\nProcess ID		Burst Time		Waiting Time		TurnAround Time\n");
+	for (i = 0; i < numOfProcesses; i++) {
+		turnAroundTime[i] = burstTime[i] + waitingTime[i];
+		total += turnAroundTime[i];
+		printf("\nProcess[%d]		%d			%d			%d\n", processes[i], burstTime[i], waitingTime[i], turnAroundTime[i]);
+	}
+
+	/* The turn around time is the additon of the burst time with the waiting time */
+	turnAroundAvg = (double)total / numOfProcesses;
+
+	printf("\nAverage Waiting Time = %f\n", waitingAvg);
+	printf("\nAverage TurnAround Time = %f\n", turnAroundAvg);
+	
 
 	closing_descriptor = close(fd);
 	if (closing_descriptor < 0)
