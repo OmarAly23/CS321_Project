d7f702a2d5bcab12494bcf23254b3b949edf0f89
OmarAly23 <aly.omar@cs.ku.edu.kw>
2021-05-02 02:02:23 +0300

    Extracted the burst time of each process from the file

--- src/nonPremptiveSJF.c
+++ src/nonPremptiveSJF.c
@@ -12,6 +12,7 @@ int main() {
 	int k, l, closing_descriptor = 0;
 	char *region;
 	char *temp;
+	char *temp2;
 	struct stat info;
 	char buff[MAXBUF];
 	bzero(buff, sizeof(buff));
@@ -31,7 +32,21 @@ int main() {
 	/* Extracting the number of processes from the file */
 	temp = strtok(buff, "\n");
 	numOfProcesses = atoi(temp);
-	printf("%d\n", numOfProcesses);
+	printf("Enter Total Number of Processes:	%d\n", numOfProcesses);
+	
+	temp2 = strcat(&buff[2], &buff[3]);
+	burstTime[0] = atoi(temp2);
+
+	temp2 = strcat(&buff[5], &buff[6]);
+	burstTime[1] = atoi(temp2);
+
+	temp2 = strcat(&buff[8], &buff[9]);
+	burstTime[2] = atoi(temp2);
+
+	for (int p = 0; p < numOfProcesses; p++) {
+		printf("Enter Burst Time For Process[%d]:	%d\n", p+1, burstTime[p]);
+		processes[p] = p+1;
+	}
 	
 
 
